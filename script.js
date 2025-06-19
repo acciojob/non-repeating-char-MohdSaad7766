1feb7e3de2d6cbd4d3d69d3866888a1709804ca1
Ansari-Mohd-Saad <157679873+MohdSaad7766@users.noreply.github.com>
2025-06-19 20:19:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	int n = str.length;
+
+	for(int i=0;i<n;i++){
+		let isFound==false {
+		}= false;
+		for(int j=i+1;j<n;j++){
+			if(str[i] == str[j]){
+				isFound = true;
+				break;
+			}
+		}
+		if(isFound==false){
+			return str[i];
+		}
+	}
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
